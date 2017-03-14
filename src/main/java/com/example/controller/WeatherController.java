8d4f766eeaf1e9576b270f5a277394816c14a36a
kozer1223 <kozerskilo@interia.pl>
2017-03-14 18:09:04 +0100

    Profile beans

--- src/main/java/com/example/controller/WeatherController.java
+++ src/main/java/com/example/controller/WeatherController.java
@@ -16,38 +16,12 @@ import java.util.List;
 @RequestMapping("/api")
 public class WeatherController {
 
-/*    @Qualifier("cute")
-    @Autowired //Injected, Named, ...
-    private WeatherService cuteWeatherService;
-
-    @Qualifier("ugly")
-    @Autowired //Injected, Named, ...
-    private WeatherService uglyWeatherService;
-
-    @Autowired
-    private Collection<WeatherService> weatherServices;*/
-
     @Autowired
     private WeatherService weatherService;
 
     @RequestMapping("/weather")
     public String getWeather() {
-        /*String weather = "";
-        for (WeatherService service : weatherServices){
-            weather += service.getWeather() + " ";
-        }
-        return weather;*/
         return weatherService.getWeather();
     }
-
-/*    @RequestMapping("/weather/cute")
-    public String getCuteWeather() {
-        return cuteWeatherService.getWeather();
-    }
-
-    @RequestMapping("/weather/ugly")
-    public String getUglyWeather() {
-        return uglyWeatherService.getWeather();
-    }*/
 
 }
--- src/main/java/com/example/service/impl/UglyWeatherServiceImpl.java
+++ src/main/java/com/example/service/impl/UglyWeatherServiceImpl.java
@@ -1,14 +1,13 @@
 package com.example.service.impl;
 
 import com.example.service.WeatherService;
-import com.example.service.impl.conditions.WindowsCondition;
-import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by Kacper on 2017-03-14.
  */
-@Conditional(value = WindowsCondition.class)
+@Profile("uglyProfile")
 @Service("ugly")
 public class UglyWeatherServiceImpl implements WeatherService {
 
